5bc85d352c49619a0da22f1d8f103725f84bf3e3
Austin Scott <austin.scott18@gmail.com>
2019-01-15 21:19:50 -0500

    pagination links and page display working properly
    Need to add search function

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -3,44 +3,17 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
+//Global variables
 const list = document.getElementsByClassName('student-item');
-
-let page = 1;
-
-
-
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
+const itemsPerPage = 10;//Hard coded to always show 10 items per page
+const firstPage = 1;    //Always start on page one. Indexing starts at 1 instead of 0.
 
 
-const showPage = (list,page) => {
-  const pageMin = page * 10 - 10;
-  const pageMax = page * 10;
+
+//Shows selected page number with the given items per page.
+const showPage = (list,selectedPage) => {
+  const pageMin = selectedPage * itemsPerPage - itemsPerPage;
+  const pageMax = selectedPage * itemsPerPage;
 
   for(let i = 0; i < list.length; i++){
     const listItem = list[i];
@@ -50,46 +23,84 @@ const showPage = (list,page) => {
       listItem.style.display = "none";
     }
   }
-  appendPageLinks(list,page);
-}
+};
 
+//Generates and adds functionality to pagination links.
+const appendPageLinks = (list) => {
+  const numPages = Math.ceil(list.length/itemsPerPage); //Number of pages to be made
 
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
-
-const appendPageLinks = (list,page) => {
-  const pageDiv = document.querySelector('.page');
+  //Create div to hold links, change class to pagination, add to page div.
   const div = document.createElement('div');
   div.className = 'pagination';
+  document.querySelector('.page').appendChild(div);
+
+  //Creates and appends ul to div
   const ul = document.createElement('ul');
-  const numPages = Math.ceil(list.length/10);
+  div.appendChild(ul);
 
-  function createElement(elementName, property, value) {
-    const element = document.createElement(elementName);
-    element[property] = value;
-    return element;
-  }
+  //Loops through the number of pages and creates a link for each
+  for(let i =0; i<numPages;i++){
+    let li = document.createElement('li');  //li to hold link
 
-  for(let i =0; i<numPages; i++){
-    const li = createElement('li');
-    const a = createElement('a','href','#');
-    a.textContent = i + 1;
-
-    if(i ==(page-1) ) {
+    //Generates link and makes the first one active
+    let a = document.createElement('a');
+    a.href = '#';
+    a.textContent = i+1;
+    if(i == (firstPage-1)){
       a.className = 'active';
     }
 
+    //Appends link to ul
     li.appendChild(a);
     ul.appendChild(li);
-    div.appendChild(li);
   }
 
-  pageDiv.appendChild(div);
-  console.log(pageDiv);
-}
+  //Selects links to be manipulated in event listener
+  const links = ul.getElementsByTagName('a');
+  console.log(links);
 
-document.addEventListener('DOMContentLoaded',(e) => {showPage(list,page)});
+  /*Event listener to change all pagination links to inactive
+    and make the clicked link active. Then shows the page for the selected link.*/
+  ul.addEventListener('click', (event) => {
+    if(event.target.tagName = 'A') {
+      for(let i =0; i <links.length; i++){
+        links[i].className = "";
+      }
+      event.target.className = "active";
+      showPage(list,event.target.textContent);
+    }
+  });
+};
+
+const searchFunction = (list) => {
+
+  //Creates div to hold search input and button
+  const searchDiv = document.createElement('div');
+  searchDiv.className = "student-search";
+
+  //input
+  const placeholder = document.createElement('input');
+  placeholder.value = "Search for students...";
+  searchDiv.appendChild(placeholder);
+
+  //Search button
+  const searchButton = document.createElement('button');
+  searchButton.textContent = "Search";
+  searchDiv.appendChild(searchButton);
+
+  //Page header
+  const header = document.querySelector('.page-header');
+  header.appendChild(searchDiv);
+
+  searchButton.addEventListener('click', (event) => {
+    
+
+  });
+
+}
+showPage(list, firstPage);  //Shows the first page
+appendPageLinks(list);  //adds pagination links
+searchFunction(list);
+
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
